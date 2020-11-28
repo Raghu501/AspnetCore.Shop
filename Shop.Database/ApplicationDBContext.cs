17e3412c716687cf2f94d7d1ec32a69b5604f314
murari <raghu.00a@gmail.com>
2020-11-28 23:01:29 +0530

    initial commit

---
+++ Shop.Database/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Models;
+
+namespace Shop.Database {
+
+    public class ApplicationDbContext : IdentityDbContext {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options):
+            base (options) { }
+
+        public DbSet<Product> Product { get; set; }
+
+    }
+
+}
